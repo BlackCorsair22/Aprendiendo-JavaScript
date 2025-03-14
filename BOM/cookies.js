6c69ba0df37def76a37a2bb6d5c898485649e184
KingSlayer <dev.figueroanicolas22@gmail.com>
2025-03-14 12:53:16 -0300

    agregando nuevas caracteristicas

--- BOM/cookies.js
+++ BOM/cookies.js
@@ -1,1 +1,29 @@
-document.cookie = "Nombre=Nicolas";
+const iniciarSesion = () => {
+    let user = prompt('Por Favor inicia sesion con tu nombre');
+
+    if(user === '' || user === undefined || !isNaN(user)) {
+        alert('Por favor ingresa un nombre valido');
+        iniciarSesion();
+    } else {
+        document.cookie = `usuario=${user}`;
+        alert('Sesión iniciada correctamente')
+    }
+}
+
+let usuario;
+
+const cookies = document.cookie.split(';'); 
+
+cookies.forEach(cookie => {
+    let propiedad = cookie.split('=')[0];
+
+    if(propiedad === 'usuario') {
+        usuario = cookie.split('=')[1];
+    }
+})
+
+if(usuario) {
+    alert(`Bienvenido ${usuario}`);
+} else {
+    alert('Por favor inicia sesión')
+}
---
+++ DOM/obtenerElementos.js
@@ -0,0 +1,19 @@
+/* 📌 getElementById
+
+   - Nos permite seleccionar un elemento por su id.
+
+   Ejemplo: 
+
+    const contenedor1 = document.getElementById('contenedor1');
+*/
+
+/* 📌 children
+
+    - Nos permite seleccionar los elementos hijos de un elemento.
+
+    Ejemplo: 
+
+    const contenedor1 = document.getElementById('contenedor1');
+
+    const hijos = contenedor1.children;
+*/
